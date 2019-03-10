541d6f47e91dc52c3ec89a3c1898f1b772304d80
cicavip <cicavip@163.com>
2019-03-10 14:01:26 +0800

    change database from localhose to 10.236.71.134 & add a Rapid deployment(快速部署数据库链接） databaselink

---
+++ main.py
@@ -0,0 +1,16 @@
+#程序主入口
+from MTGM.gm_main import *
+
+if __name__ == '__main__':
+	app = QtWidgets.QApplication(sys.argv)
+	main_ui = Parentwindow()
+	"""窗口UI"""
+	mtgm_ui = MTGM_window()
+	showtodaytask_ui = Dialog_showtodaytask()
+	"""按钮动作"""
+	btn = main_ui.main_ui.mtgm_btn
+	btn.clicked.connect(mtgm_ui.show)
+	btn_3 = mtgm_ui.showtodaytask
+	btn_3.clicked.connect(showtodaytask_ui.show)
+	main_ui.show()
+	sys.exit(app.exec_())
